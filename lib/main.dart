cdc8552f49981ce87d0c68058e76702978395cf7
FunnyL00L <143920474+FunnyL00L@users.noreply.github.com>
2024-04-18 16:29:46 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:book_grocer/common/color_extenstion.dart';
-import 'package:book_grocer/view/main_tab/main_tab_view.dart';
+import 'package:flutter_application_1/model/petani.dart';
+import 'package:flutter_application_1/sistem/class.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: TColor.primary,
-        fontFamily: 'SF Pro Text',
-      ),
-      home: const MainTabView(),
-    );
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<List<Petani>> futurePetani;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePetani = fetchPetani();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    var materialApp = MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Petani>>(
+            future: futurePetani,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // Display a loading indicator while waiting for the future
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                // Display an error message if there's an error
+                return Text('Error: ${snapshot.error}');
+              } else if (snapshot.hasData) {
+                // Display the list of Petani if data is available
+                final List<Petani> petaniList = snapshot.data!;
+                return ListView.builder(
+                  itemCount: petaniList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    // Display each Petani's name
+                    return Text('${petaniList[index].nama}');
+                  },
+                );
+              } else {
+                // Default case: Display a message when there's no data
+                return const Text('No data available');
+              }
+            },
+          ),
+        ),
+     ),);
+    return materialApp;
+}
 }
